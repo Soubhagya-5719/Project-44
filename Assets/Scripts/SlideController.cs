fef21738d5869bde8b8520b108ee26f2025def79
swetanjal <swetanjal.dutta@research.iiit.ac.in>
2020-11-08 13:48:32 +0530

    Add slides projection.

---
+++ Assets/Scripts/SlideController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideController : MonoBehaviour
+{
+    public Material Page1;
+    public Material Page2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<MeshRenderer> ().material = Page1;    
+    }
+}
